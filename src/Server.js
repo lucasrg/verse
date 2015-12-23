6bf3d45f2016a0e1e74e33cd951f8aa7d176dbf8
Lucas Goraieb <lucas.goraieb@gmail.com>
2015-12-23 15:09:39 -0800

    Basic server side functionality working

---
+++ src/Server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Utils = {
+  isArray: ('isArray' in Array) ? Array.isArray :
+      function (value) {
+          return Object.prototype.toString.call(value) === '[object Array]';
+      }
+};
+
+var Server = {
+  render: function (input, context) {
+    if (typeof input == 'object') {
+      if (Utils.isArray(input)) {
+        var out = '';
+        input.forEach(function (item) {
+          out += Server.render(item, context)
+        })
+        return out;
+      } else {
+        var tagDefinition = '<'+input.tag;
+        var innerHTML = '';
+        Object.keys(input).forEach(function(key) {
+          var val = input[key];
+          if (key == 'tag' || key == 'listen') {
+            // Ignore
+          } else if (key == 'render') {
+            innerHTML = Server.render(val, context);
+          } else if (key == 'className') {
+            tagDefinition += ' class="'+val+'"';
+          } else {
+            tagDefinition += ' '+key+'="'+val+'"';
+          }
+        });
+        tagDefinition += '>';
+        return tagDefinition + innerHTML + '</'+input.tag+'>';
+      }
+    } else if (typeof input == 'function') {
+      return this.render(input(context), context);
+    } else if (typeof input != 'undefined' && input != null){
+      return input.toString();
+    }
+    return '';
+  }
+}
+
+module.exports = Server;
---
+++ test/index.js
@@ -0,0 +1,51 @@
+var should = require('chai').should(),
+    universal = require('../index'),
+    Server = universal.Server;
+
+describe('#Server', function() {
+  it('render static hello world', function() {
+    var template = {
+      tag: 'html', render: [
+        {tag:'head'},
+        {tag:'body', render: {tag:'h1', render:'Hello World!'}}
+      ]
+    };
+    Server.render(template).should.equal('<html><head></head><body><h1>Hello World!</h1></body></html>');
+  });
+
+  it('render context hello world', function() {
+    var helloWorldContext = {
+      title: 'Hello World?',
+      nested: {subtitle: 'Hello Hello'}
+    };
+    var template = function(context) {
+      return {
+        tag: 'html', render: [
+          {tag:'head'},
+          {tag:'body', render: [
+              {tag:'h1', className: 'title', render: function (ctx) {
+                  return ctx.title
+                }
+              },
+              {tag:'h2', render: context.nested.subtitle}
+            ]
+          }
+        ]
+      }
+    };
+    Server.render(template, helloWorldContext).should.equal('<html><head></head><body><h1 class="title">Hello World?</h1><h2>Hello Hello</h2></body></html>');
+  });
+
+  it('render components', function() {
+    var HelloComponent = { tag: 'span', render: 'Hello' };
+    var WorldComponent = function(props) {
+      return { tag: 'span', render: props.name }
+    }
+    var HelloWorldComponent = function(context){
+      return {tag:'h1', render: [ HelloComponent, WorldComponent({name: context.prefix}), WorldComponent({name: 'World'}) ]}
+    }
+    var helloWorldContext = {prefix: 'Big Old'};
+    Server.render(HelloWorldComponent, helloWorldContext).should.equal('<h1><span>Hello</span><span>Big Old</span><span>World</span></h1>');
+  });
+
+});
